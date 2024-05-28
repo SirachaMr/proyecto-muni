ccd6c22c28a58efac8664220a7382895a73a89c2
Tobias <tardiol63@gmail.com>
2024-05-28 12:41:02 -0300

    cambie logo

--- proyecto-muni/ptro/src/App.jsx
+++ proyecto-muni/ptro/src/App.jsx
@@ -16,13 +16,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
