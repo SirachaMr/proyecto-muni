5e3b40e9bb8c79e5e98487547fc21b7ee58face9
Tobias <tardiol63@gmail.com>
2024-05-28 12:25:10 -0300

    edgar

--- proyecto-muni/ptro/src/App.jsx
+++ proyecto-muni/ptro/src/App.jsx
@@ -8,15 +8,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="header">
+        <h1 class="Titulo">Turnero</h1>
       </div>
-      <h1>Vite + React</h1>
+     
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
