3f94d7bc2fb876d5528dc85e6b86b556b6f5d1ca
Tobias <tardiol63@gmail.com>
2024-05-28 12:59:05 -0300

    esgaueti

--- proyecto-muni/ptro/src/App.jsx
+++ proyecto-muni/ptro/src/App.jsx
@@ -4,19 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div id="header">
+      <div class="header">
         <h1 class="Titulo">Turnero</h1>
       </div>
      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      <div>
+
       </div>
      
     </>
