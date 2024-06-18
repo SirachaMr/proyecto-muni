bb6d3593c9b8944fa7c47239b88fea52d8229500
maxi <maxbrax144@gmail.com>
2024-06-18 12:14:32 -0300

    pancreas

--- proyecto-muni/ptro/src/wizard/pasos/Paso1.jsx
+++ proyecto-muni/ptro/src/wizard/pasos/Paso1.jsx
@@ -8,7 +8,8 @@ export default function Paso1() {
             <p class="lista">¿Cómo reservar tu turno?</p>
             <ol class="lista">
               <li>Completá tu información personal y una casilla de correo electrónico para recibir los detalles del turno solicitado.</li>
-              <li>Seleccioná el trámite que tenés que realizar.</li>
+              <li>Indique su obra social con toda la información requerida.</li>
+              <li>Escoga la especialidad</li>
               <li>Elegí un día del calendario y un horario que se encuentre disponible.</li>
             </ol>
         </div>
